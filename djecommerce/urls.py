a88b3fa93b3cb62fa8fb52df575bd51e17118795
Your Name <abhilashbhattacharya17@gmail.com>
2023-07-15 15:46:25 +0530

    modified:   core/models.py 	modified:   db.sqlite3 	modified:   djecommerce/urls.py 	modified:   static_in_env/css/bootstrap.css 	modified:   static_in_env/css/mdb.css 	modified:   static_in_env/css/mdb.min.css 	new file:   static_in_env/img/imagesfav/android-chrome-192x192.png 	new file:   static_in_env/img/imagesfav/android-chrome-512x512.png 	new file:   static_in_env/img/imagesfav/apple-touch-icon.png 	new file:   static_in_env/img/imagesfav/browserconfig.xml 	new file:   static_in_env/img/imagesfav/favicon-16x16.png 	new file:   static_in_env/img/imagesfav/favicon-32x32.png 	new file:   static_in_env/img/imagesfav/favicon.ico 	new file:   static_in_env/img/imagesfav/mstile-150x150.png 	new file:   static_in_env/img/imagesfav/oie_495jJ0NvDCGN.png 	new file:   static_in_env/img/imagesfav/site.webmanifest 	modified:   templates/base.html 	modified:   templates/footer.html 	modified:   templates/home.html 	modified:   templates/navbar.html 	modified:   templates/product.html

--- djecommerce/urls.py
+++ djecommerce/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('core.urls', namespace='core'))
+   
 ]
 
 if settings.DEBUG:
